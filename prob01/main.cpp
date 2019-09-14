f48efa07bc11a152f729713f80a69a2fe11cfc32
Brian Smetana <54484285+brian21a@users.noreply.github.com>
2019-09-14 13:56:26 -0700

    Update main.cpp

--- prob01/main.cpp
+++ prob01/main.cpp
@@ -1,3 +1,4 @@
+
 // make sure the average includes decimal digits
 
 #include <iostream>
@@ -8,15 +9,15 @@ int main()
   // then print both the char and the short
 
   char my_char1 = 'J';
-  short my_short1 = ;
-  std::cout << "The ASCII value for " << my_char1 << " is " << my_short1 << std::endl;
+  short my_short1 = static_cast<int>(my_char1);
+  std::cout << "the ASCII value for" << my_char1 << " is " << my_short1 << std::endl;
 
   std::cout << std::endl; // blank line to separate output from the different exercises
 
   // TODO#2: add one to the character to see what gets printed out
 
   char my_char2 = 'C';
-  my_char2 = ;
+  my_char2 = 'C'+1;
   std::cout << "Char1: " << my_char2 << std::endl;
 
   std::cout << std::endl; // blank line to separate output from the different exercises
@@ -26,10 +27,13 @@ int main()
   // longer alphabetic
 
   char my_char3 = 'z';
-  my_char3 = ;
+  my_char3 = 'z'+1;
   std::cout << "Char2: " << my_char3 << std::endl;
 
   std::cout << std::endl; // blank line to separate output from the different exercises
+
+  // my_char3 is no longer alphabetic because 'z' is the last character in the alphabet
+
 
   // TODO#4: print each number, then add one to each number and print it again
   // also add a comment to explain why my_short2 value became negative after
@@ -42,17 +46,19 @@ int main()
 
   std::cout << "Short (" << sizeof(my_short2) << " bytes): "
     << std::dec << my_short2 << std::hex << " (0x" << my_short2 << ')';
-  my_short2 = ;
+  my_short2 = my_short2+1;
   std::cout << ", after adding 1: " << std::dec << my_short2
     << std::hex << " (0x" << my_short2 << ")\n";
 
   std::cout << "Int (" << sizeof(my_short2) << " bytes): "
     << std::dec << my_int2 << std::hex << " (0x" << my_int2 << ')';
-  my_int2 = ;
+  my_int2 = my_int2+1;
   std::cout << ", after adding 1: " << std::dec << my_int2
     << std::hex << " (0x" << my_int2 << ")\n";
 
   std::cout << std::endl; // blank line to separate output from the different exercises
+
+  // my_short2 was the maximum value for data type short
 
   // TODO#5: print each number, then add one to each number and print it again
   // also add a comment to explain why my_short2 value became negative after
@@ -63,15 +69,17 @@ int main()
 
   std::cout << "Unsigned short (" << sizeof(my_short3) << " bytes): "
     << std::dec << my_short3 << std::hex << " (0x" << my_short3 << ')';
-  my_short3 = ;
+  my_short3 = my_short3+1;
   std::cout << ", after adding 1: " << std::dec << my_short3
     << std::hex << " (0x" << my_short3 << ")\n";
 
   std::cout << "Same value as int (" << sizeof(my_int3) << " bytes): "
     << std::dec << my_int3 << std::hex << " (0x" << my_int3 << ')';
-  my_int3 = ;
+  my_int3 = my_int3+1;
   std::cout << ", after adding 1: " << std::dec << my_int3
     << std::hex << " (0x" << my_int3 << ")\n";
+
+    // my_short3 is 0 because the maximum value for unsigned short is 65535.
 
   // ** not required, but for more practice, feel free to add code below which **
   // ** tests underflow and overflow using other data types **
