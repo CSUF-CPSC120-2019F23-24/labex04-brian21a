d7137cbde720ae2b4cddf7182b3d3592ac30f0ea
brian21a <sourcream@csu.fullerton.edu>
2019-09-14 14:38:36 -0700

    yearly sales predictions

--- prob03/main.cpp
+++ prob03/main.cpp
@@ -1,1 +1,15 @@
 // Sales prediction
+
+#include <iostream>
+
+int main()
+{
+const long TOTAL_SALES = 2103419277;
+const long long NEXT_YEAR_SALES = TOTAL_SALES*1.18;
+
+std::cout << "Last year's prediction were $" << TOTAL_SALES << std::endl;
+std::cout << "This year's prediction $" << NEXT_YEAR_SALES << std::endl;
+
+
+  return 0;
+}
